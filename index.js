6b2632dfe9d424ea0cad56961090ad48419219e1
yorgis711 <105851696+yorgis711@users.noreply.github.com>
2023-03-18 23:53:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+
+
+
+app.get('/api', (req, res) => {
+
+    const request = require('request');
+    request('https://api.open-meteo.com/v1/forecast?latitude=37.98&longitude=23.73&hourly=temperature_2m', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body)
+        } else {
+            res.send({ message: "There was an error during your request" })
+        }
+
+    })
+
+});
+
+module.exports = app;
+
