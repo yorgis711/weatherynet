20a74c9a0dedd689bf6aa9671ae0597752526e62
yorgis711 <giorgos@yorgis.net>
2025-02-01 22:33:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const htmlContent = `
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modern Weather Forecast</title>
+    <title>Weather Forecast</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <style>
